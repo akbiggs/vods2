64e466f85c8f779bfc413b9f18fd85151c546d86
lumi-sg <shaicloud@gmail.com>
2025-08-23 17:30:51 -0400

    incremental pagination aesthetics

--- app.py
+++ app.py
@@ -50,7 +50,11 @@ def home_page():
         per_page=per_page,
         total=total,
         inner_window=2,
-        outer_window=0
+        outer_window=0,
+        prev_label='<&nbsp;&nbsp;&nbsp;Previous',
+        next_label='Next&nbsp;&nbsp;&nbsp;>',
+        css_framework='bootstrap5',
+        bs_version=5
     )
 
     return render_template(
@@ -94,7 +98,11 @@ def search_page():
         per_page=per_page,
         total=total,
         inner_window=2,
-        outer_window=0
+        outer_window=0,
+        prev_label='<&nbsp;&nbsp;&nbsp;Previous',
+        next_label='Next&nbsp;&nbsp;&nbsp;>',
+        css_framework='bootstrap5',
+        bs_version=5
     )
     
     return render_template("home.jinja2", vods=vods, c1=c1, c2=c2, p1=p1, p2=p2, event=event, rank=rank, channels=get_channels(), is_search=True, pagination=pagination)
