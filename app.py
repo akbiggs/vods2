712ce54c457832da057b3ed3304a599641bb69e2
lumi-sg <shaicloud@gmail.com>
2026-01-25 22:45:50 -0500

    some mobile fixes

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def search_page():
         next_label='Next&nbsp;&nbsp;&nbsp;>',
         css_framework='bootstrap5',
         bs_version=5,
-        display_msg="{start} - {end} of {total}"
+        display_msg="{start} - {end} / {total}"
     )
     
     return render_template("home.jinja2", vods=vods, c1=c1, c2=c2, p1=p1, p2=p2, event=event, rank=rank, channels=get_channels(), is_search=True, pagination=pagination)
